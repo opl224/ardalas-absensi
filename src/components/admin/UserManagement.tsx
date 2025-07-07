d4c240e550f18732360f394c9af19ed7da6f2408
Opal Aja <opank2441@gmail.com>
2025-07-07 16:21:31 +0000

    di manajemen pengguna pindahkan kebawah judul / title untuk icon unduh d

--- src/components/admin/UserManagement.tsx
+++ src/components/admin/UserManagement.tsx
@@ -6,7 +6,7 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Search, Download, Filter, FilePen, Trash2 } from 'lucide-react';
+import { Search, Download, FilePen, Trash2 } from 'lucide-react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/lib/firebase';
 import { LottieLoader } from '../ui/lottie-loader';
@@ -62,18 +62,17 @@ export function UserManagement() {
 
   return (
     <div className="bg-gray-50 dark:bg-zinc-900 p-4">
-      <header className="flex items-center justify-between mb-4">
+      <header className="mb-4">
         <h1 className="text-xl font-bold text-foreground">Manajemen Pengguna</h1>
-        <div className="flex items-center gap-2">
-          <Button variant="outline" size="icon" className="bg-card">
-            <Download className="h-4 w-4" />
-          </Button>
-          <Button variant="outline" size="icon" className="bg-card">
-            <Filter className="h-4 w-4" />
-          </Button>
-          <Button>Tambah Pengguna</Button>
-        </div>
       </header>
+
+      <div className="flex items-center gap-2 mb-4">
+        <Button variant="outline">
+            <Download className="mr-2 h-4 w-4" />
+            Unduh
+        </Button>
+        <Button className="ml-auto">Tambah Pengguna</Button>
+      </div>
 
       <div className="relative mb-4">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
