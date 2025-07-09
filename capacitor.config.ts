1fb69a6551e0543c9a0d7baac83ebd66a59196b0
Opal Aja <opank2441@gmail.com>
2025-07-09 04:25:47 +0000

    bagaimana cara download sdk 34 atau 33 dan di letakan di folder mana di

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -8,7 +8,18 @@ const config: CapacitorConfig = {
     androidScheme: 'https'
   },
   android: {
-    // Variables are now defined in android/gradle.properties
+    // Variables are now defined here to be injected into gradle files
+    buildToolsVersion: '34.0.0',
+    compileSdkVersion: 34,
+    targetSdkVersion: 34,
+    minSdkVersion: 22,
+    androidXCoreVersion: '1.13.1',
+    androidXAppCompatVersion: '1.7.0',
+    androidXBrowserVersion: '1.8.0',
+    androidXActivityVersion: '1.9.1',
+    googlePlayServicesLocationVersion: '21.3.0',
+    firebaseMessagingVersion: '24.0.0',
+    androidxCoordinatorLayoutVersion: '1.2.0',
   }
 };
 
